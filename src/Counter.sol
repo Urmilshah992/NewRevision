20290c72e47e0ab1b0d54d6a443fb8bdb04b9031
Urmil Shah <160323398+Urmilshah992@users.noreply.github.com>
2024-12-16 17:28:32 +0000

    Statfull Fuzzing Added

--- src/Counter.sol
+++ src/Counter.sol
@@ -4,11 +4,18 @@ pragma solidity ^0.8.13;
 contract Counter {
     uint256 public number;
     uint256 public data = 0;
+    uint256 public hiddenvalue = 0;
 
     function shouldAlwaysZero(uint256 dat1) public{
         if(dat1 == 2){
-            data = dat1;  
+            data = 1;  
         }
+        if(hiddenvalue == 7){
+            data = 1;
+        }
+
+        hiddenvalue = dat1;
+
     }
 
     function setNumber(uint256 newNumber) public {
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
+import {StdInvariant} from "forge-std/StdInvariant.sol";
 
-contract CounterTest is Test {
+contract CounterTest is StdInvariant, Test { 
     Counter public counter;
 
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
+        targetContract(address(counter));
+        counter.setNumber(0);      
     }
 
     function test_Increment() public {
@@ -22,9 +24,15 @@ contract CounterTest is Test {
         assertEq(counter.number(), x);
     }
 
-    function testShouldAlwaysZero(uint256 y) public {
+    function testShouldAlwaysZeroFuzz(uint256 y) public {
     counter.shouldAlwaysZero(y);
     assert(counter.data() == 0);
+    }
+
+
+    function invariant_testXy() public view {
+        assert(counter.data() == 0);
+
     }
 }
 
