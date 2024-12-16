a5c5c1d09f64729fc56f49ccded1abe5c525d1ee
Urmil Shah <160323398+Urmilshah992@users.noreply.github.com>
2024-12-16 19:24:14 +0000

    ERC20 Tokan Contract Add

---
+++ src/Mytoken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Mytoken is ERC20 { 
+    constructor() ERC20("Urmil","UBS") {
+
+    }
+    
+} 
+
+
