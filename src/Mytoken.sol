f3206a935201f127bba37a481fb3dcaee4f8df77
Urmil Shah <160323398+Urmilshah992@users.noreply.github.com>
2024-12-17 18:23:59 +0000

    Constant and Immutable Variable

--- src/Mytoken.sol
+++ src/Mytoken.sol
@@ -6,7 +6,7 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract Mytoken is ERC20 { 
      uint256 public number1 = 1;
         uint256  public number2 = 2 ;
-        uint256 public number3  = 3;
+        uint256 public constant number3  = 3;
     constructor() ERC20("Urmil","UBS") {
        
 
