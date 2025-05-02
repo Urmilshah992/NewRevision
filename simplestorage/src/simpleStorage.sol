8685b09dfa591652f7f615cfae5ae37b81547acd
Urmil Shah <160323398+Urmilshah992@users.noreply.github.com>
2025-05-02 17:14:07 +0000

    simpeStorage Program

--- simplestorage/src/simpleStorage.sol
+++ simplestorage/src/simpleStorage.sol
@@ -1,5 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
-contract simplestorage{
-    
+
+contract SimpleStorage {
+
+    uint256 public myFavoriteNumber;
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+    function viewThePerson(string memory _name) public view returns(uint256){
+        return nameToFavoriteNumber[_name];
+
+    } 
 }
