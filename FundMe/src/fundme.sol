547e5ac28afb121863c7cb218780932954a3e48b
Urmil Shah <160323398+Urmilshah992@users.noreply.github.com>
2025-06-17 15:18:14 +0000

    Custom Error Added

--- FundMe/src/fundme.sol
+++ FundMe/src/fundme.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 import {PriceConvertor} from "./PriceConvertor.sol";
+error NotOwner();
 contract FundMe{
     using PriceConvertor for uint256;
     uint256 public constant MINIMUMUSD = 5e18;
@@ -33,7 +34,10 @@ contract FundMe{
     }   
 
     modifier onlyOwner(){
-        require(msg.sender == i_owner, "Only owner can call this function");
+        // require(msg.sender == i_owner, "Only owner can call this function");
+        if(msg.sender != i_owner) {
+            revert NotOwner();
+        }
         _;
     }
 
