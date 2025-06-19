8d9c64d99a05a2f5c7639370c7bdc46bc21e9258
Urmil Shah <160323398+Urmilshah992@users.noreply.github.com>
2025-06-19 16:13:06 +0000

    Deleteing the Odd files

--- simplestorage/src/addFivestorage.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.29;
-
-import {SimpleStorage} from "./simpleStorage.sol";
-contract addFiveStorage is SimpleStorage {
-
- function store(uint256 _favoriteNumber) public override {
-        myFavoriteNumber = _favoriteNumber + 5;}
-}
