9ba4fc8db3013f9bd20459969a1c586acf7e6ab7
Urmil Shah <160323398+Urmilshah992@users.noreply.github.com>
2025-06-26 09:27:04 +0530

    Funde Contract Refctoring

--- FundMe/src/fundme.sol
+++ FundMe/src/fundme.sol
@@ -2,30 +2,35 @@
 pragma solidity ^0.8.29;
 
 import {PriceConvertor} from "./PriceConvertor.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+
 error FundMe_NotOwner();
 
-contract FundMe{
+contract FundMe {
 
     using PriceConvertor for uint256;
     uint256 public constant MINIMUMUSD = 5e18;
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public fundersAmount;
     address public immutable  i_owner;
+    AggregatorV3Interface public s_priceFeed;
     
-    constructor(){
+    constructor(address priceFeedAddress) {
         i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
     function fund() public payable{
         //1e18 = 1ETH = 1000000000000000000 wei
         //1e18 = 10000000000 Gwei (Gas Cost Count)
-        require(msg.value.getConversionRate() > MINIMUMUSD, "You need to spend more ETH!");
+        require(msg.value.getConversionRate(s_priceFeed) > MINIMUMUSD, "You need to spend more ETH!");
         funders.push(msg.sender);
         fundersAmount[msg.sender] += msg.value;
     }
 
     function getversion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
+
+        return s_priceFeed.version();
         
     }
     function withdraw() public onlyOwner {
--- FundMe/test/FundMeTest.t.sol
+++ FundMe/test/FundMeTest.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.29;
 
 import {FundMe} from "../src/fundme.sol";
 import {Test} from "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
 
 contract FundMeTest is Test(){
     FundMe fundme;
@@ -19,19 +20,26 @@ contract FundMeTest is Test(){
         assertEq(fundme.i_owner(), address(this));
     }
 
-    function testwidraw() public{
-        //arrange some fund
-        uint256 fundamount = 10e18; // 10 ETH
-        vm.deal(address(this), fundamount); // Give this contract 10 ETH
-        fundme.fund{value: fundamount}(); // Fund the contract
-        uint256 initialBalance = address(this).balance; // Get the initial balance of the contract
-        //act
-        fundme.withdraw(); // Withdraw the funds
-        //assert
-        uint256 finalBalance = address(this).balance; // Get the final balance of the contract
-        assertEq(finalBalance, initialBalance + fundamount); // Check if the final balance is
-        // equal to the initial balance plus the fund amount
-        assertEq(fundme.fundersAmount(address(this)), 0); // Check if the amount funded by this contract is reset to 0
+    // function testwidraw() public{
+    //     //arrange some fund
+    //     uint256 fundamount = 10e18; // 10 ETH
+    //     vm.deal(address(this), fundamount); // Give this contract 10 ETH
+    //     fundme.fund{value: fundamount}(); // Fund the contract
+    //     uint256 initialBalance = address(this).balance; // Get the initial balance of the contract
+    //     //act
+    //     fundme.withdraw(); // Withdraw the funds
+    //     //assert
+    //     uint256 finalBalance = address(this).balance; // Get the final balance of the contract
+    //     assertEq(finalBalance, initialBalance + fundamount); // Check if the final balance is
+    //     // equal to the initial balance plus the fund amount
+    //     assertEq(fundme.fundersAmount(address(this)), 0); // Check if the amount funded by this contract is reset to 0
+    // }
+
+
+    function testgetversionAccurate() public view{
+        uint256 version = fundme.getversion();
+        console.log(version);
+        assertEq(version, 4); // Check if the version is 4
     }
 
 }
