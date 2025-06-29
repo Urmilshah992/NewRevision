7efacd23128104e8b223b896821d993e387d1f67
Urmil Shah <160323398+Urmilshah992@users.noreply.github.com>
2025-06-29 21:11:33 +0530

    constructor Set for ChainID

--- FundMe/script/HelperConfig.s.sol
+++ FundMe/script/HelperConfig.s.sol
@@ -11,6 +11,16 @@ contract HelperConfig is Script {
 
     NetworkConfig public activeNetworkConfig;
 
+    constructor() {
+        if(block.chainid == 11155111) {
+            //Sepolia
+            activeNetworkConfig = getSepoliaEthConfig();
+        }
+        else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory){
         //priceFeedAddress of the Sepolia ETH/USd Price Feed
         NetworkConfig memory seploiaConfig = NetworkConfig({priceFeedAddress: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
