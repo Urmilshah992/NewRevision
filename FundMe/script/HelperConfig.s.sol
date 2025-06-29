126e5a2bda1cc9dff3df6d7c82bedeaaf52fca7d
Urmil Shah <160323398+Urmilshah992@users.noreply.github.com>
2025-06-29 21:09:17 +0530

    HelperConfig Functions ready

--- FundMe/script/HelperConfig.s.sol
+++ FundMe/script/HelperConfig.s.sol
@@ -4,4 +4,21 @@ import {Script} from "forge-std/Script.sol";
 
 contract HelperConfig is Script {
     
+    struct NetworkConfig{
+        address priceFeedAddress; //Eth/USD
+
+    } 
+
+    NetworkConfig public activeNetworkConfig;
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory){
+        //priceFeedAddress of the Sepolia ETH/USd Price Feed
+        NetworkConfig memory seploiaConfig = NetworkConfig({priceFeedAddress: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        return seploiaConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory){
+        //priceFeedAddress of the Anvil ETH/USD Price Feed
+
+    }
 }
