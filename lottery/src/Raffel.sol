23f4f4851ee02ccd6e4e7b103f6e7e6faa83d972
Urmil Shah <160323398+Urmilshah992@users.noreply.github.com>
2025-07-13 22:41:33 +0530

    Basic Script and Contract added

--- lottery/src/Raffel.sol
+++ lottery/src/Raffel.sol
@@ -1,7 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >0.8.29;
 
+/**
+ * @title Raffel
+ * @author Urmil Shah
+ * @notice This contract is Sample Raffel Contract
+ */
 contract Raffel{
 
-    
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee){
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffel() public payable{}
+
+    function pickWiner() public {}
+
 }
