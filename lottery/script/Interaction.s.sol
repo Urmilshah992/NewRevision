224dbe43e466395598c5b8b00ad3bb5803549a70
Urmil Shah <160323398+Urmilshah992@users.noreply.github.com>
2025-08-31 18:29:38 +0530

    CreateSubscription Contract

--- lottery/script/Interaction.s.sol
+++ lottery/script/Interaction.s.sol
@@ -1,8 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.29;
 
-import {Script} from "forge-std/Script.sol";
+import {Script,console} from "forge-std/Script.sol";
+import {VRFCoordinatorV2_5Mock} from  "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+
+
+
 
 contract CreateSubscription is Script{
+
+    function createSubscriptionUsingPublic() public{
+        HelperConfig helperconfig = new HelperConfig();
+        address vrfCoordinator = helperconfig.getConfig().vrfCoordinator;
+        createSubscription(vrfCoordinator);
+    }
+
+    function createSubscription(address vrfCoordinator) public {
+        console.log("Creating subscription on vrfCoordinator:",vrfCoordinator);
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock(vrfCoordinator).createSubscription();
+        vm.stopBroadcast();
+
+    }
+    function run() public{
+        createSubscriptionUsingPublic(); 
+
+    }
 
 }
